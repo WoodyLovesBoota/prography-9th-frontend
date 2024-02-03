eca9677240de4999af9d32d53a95d68dc17bd801
woody <woodylovesboota@gmail.com>
2024-02-03 17:07:37 +0900

    GNB 홈버튼 추가

--- src/Routes/Home.tsx
+++ src/Routes/Home.tsx
@@ -2,7 +2,7 @@ import styled from "styled-components";
 import { ICategories, ICategory, getCategories } from "../api";
 import { useQuery } from "react-query";
 import { useEffect, useState } from "react";
-import { useLocation } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
 import CategoryList from "../Components/CategoryList";
 import MealsContainer from "../Components/MealsContainer";
 
@@ -17,6 +17,8 @@ const Home = () => {
 
   const { data, isLoading } = useQuery<ICategories>(["categories", "cate"], () => getCategories());
 
+  const navigate = useNavigate();
+
   useEffect(() => {
     if (data) {
       let tempCate = [...data.categories];
@@ -29,10 +31,15 @@ const Home = () => {
     setFoods(keyword?.split(","));
   }, [keyword]);
 
+  const onLogoClick = () => {
+    navigate("/");
+    window.location.reload();
+  };
+
   return (
     <Wrapper>
       <Header>
-        <Logo src="./logo.png" />
+        <Logo src="./logo.png" onClick={onLogoClick} />
       </Header>
       {isLoading || !sortedCate ? (
         <Loader></Loader>
@@ -61,14 +68,19 @@ const Header = styled.header`
   position: fixed;
   top: 0px;
   padding: 10px 50px;
-  background: rgba(255, 255, 255, 0.05);
+  background: rgba(255, 255, 255, 0.3);
   border-radius: 16px;
   box-shadow: 0 4px 30px rgba(0, 0, 0, 0.05);
   backdrop-filter: blur(5px);
   -webkit-backdrop-filter: blur(5.9px);
+  display: flex;
+  align-items: center;
 `;
 
-const Logo = styled.img``;
+const Logo = styled.img`
+  cursor: pointer;
+  width: 100px;
+`;
 
 const Loader = styled.div``;
 
